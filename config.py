97a400390aff075e97558acc7ae8b614b1b33a2d
mallumovieleech <88672268+mallumovieleech@users.noreply.github.com>
2024-12-25 01:52:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,14 +3,14 @@ import os
 
 
 # Required Variables Config
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+API_ID = int(os.environ.get("API_ID", "23858762"))
+API_HASH = os.environ.get("API_HASH", "d4d6e9a9e553759e45f59c2e74c8f809")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7945308877:AAEImD7zmngAKeF554rG0Cqf0sU9R5FD26o")
+ADMIN = int(os.environ.get("ADMIN", "561339410"))
 
 
 # Premium 4GB Renaming Client Config
-STRING_SESSION = os.environ.get("STRING_SESSION", "")
+STRING_SESSION = os.environ.get("STRING_SESSION", "BQFsDkoAkHUUZ0VYWv2zbVS03LrzePXg5U0mpwaIbXZQdXJhjHuDZVNz6X6SYVG1xpIYXm5SPyB8X5TZ45PpJdXEf3HEEhMyeXR5KneaWWQ6ZniX7DjSnE5YHkwR3V90sFC2OcWiuH3qDhr8pUSVkrJE6sGaDrhRZrCFpClPdhpJgpGRRntGwYSvFs4nBYsVb8oCu-avva6ff3-WKB4yV1jkmLHr5rt0QZ38K0sMkDtwbzrY4Dfg57XYWFMNsIp5guusWVKmPoos5FWI_jBQ5cjBLnH3Sa6GIrllxm8j2nS9YDCsINvr2IXLsvXmVZPmuV-4GEkYwO5fN5zC4FKIp-EhZxiJawAAAAHnBYTeAA")
 
 
 # Log & Force Channel Config
@@ -19,8 +19,8 @@ LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
 
 
 # Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "madflixbotz")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://damon:damon@cluster0.dxbn6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 
 # Other Variables Config
